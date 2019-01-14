13f56571530df183cfcb66351f356b89209a4ba8
DATBOI <alexsimons9999@gmail.com>
2019-01-13 18:09:28 -0600

    Well I am now running spring boot as compute one

---
+++ compute-app-one/src/main/kotlin/io/acari/computeappone/ComputeAppOneApplication.kt
@@ -0,0 +1,12 @@
+package io.acari.computeappone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ComputeAppOneApplication
+
+fun main(args: Array<String>) {
+  runApplication<ComputeAppOneApplication>(*args)
+}
+
